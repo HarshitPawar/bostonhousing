872e3fb1f9d636cdf9213c0079abc7c5b01d3706
Harshit Pawar <harshitpawar2306@gmail.com>
2024-02-25 19:39:56 +0530

    This is first commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+# from crypt import methods
 from flask import Flask, request, app, jsonify, url_for, render_template
 import pickle
 import os
@@ -24,6 +25,16 @@ def predict_api():
     output=regmodel.predict(new_data)
     print(output[0])
     return jsonify(output[0])
+
+@app.route('/predict', methods=['POST'])
+
+def predict():
+    data=[float(x) for x in request.form.values()]
+    final_input=scaler.transform(np.array(data).reshape(1,-1))
+    print(final_input)
+    output=regmodel.predict(final_input)[0]
+    return render_template("home.html", prediction_text="the house price predictin is {}".format(output))
+
 
 
 if __name__ == "__main__":
